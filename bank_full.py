78f3d7287bec18fdd561f6e638867e76dbe7bba0
Deyner Castro <dcastro31@cuc.edu.co>
2022-04-29 21:14:01 -0500

    Creacion y organizacion de archivos .csv

---
+++ bank_full.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 29 20:13:49 2022
+
+"""
+import numpy as np
+import pandas as pd
+
+url = 'bank-full.csv'
+data = pd.read_csv(url)
+
+# Tratamiento de data
+
+
---
+++ diabetes.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 29 20:40:52 2022
+
+"""
+
+import numpy as np
+import pandas as pd
+
+url = 'diabetes.csv'
+data = pd.read_csv(url)
+
+# Tratamiento de data
