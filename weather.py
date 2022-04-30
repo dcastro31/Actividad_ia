4410b2f9e3f7ffd602af1e4bb0dd27405f53610d
Deyner Castro <dcastro31@cuc.edu.co>
2022-04-29 22:12:03 -0500

    Organizacion de datasets

---
+++ weather.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 29 21:33:05 2022
+
+"""
+import numpy as np
+import pandas as pd
+
+url = 'weatherAUS.csv'
+data = pd.read_csv(url)
+
+# Tratamiento de data
